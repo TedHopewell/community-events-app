c3da4ca7243113db7869245a0ca9b928e42017de
TedHopewell <hopewellmaome@gmail.com>
2025-09-28 02:24:06 +0200

    Second commit, currently working on the signin/signup page then set up authenticatio

--- src/loginpage.tsx
+++ src/loginpage.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View,TextInput,TouchableOpacity,Image,Dimensions, KeyboardAvoidingView, } from 'react-native';
+import LinearGradient from "react-native-linear-gradient";
 
 // import google from "./assets/google.png";
 // import { StatusBar } from 'expo-status-bar';
@@ -8,24 +9,46 @@ import { StyleSheet, Text, View,TextInput,TouchableOpacity,Image,Dimensions, Key
 export default function Loginpage() {
   return (
     <KeyboardAvoidingView style={styles.container} behavior="padding">
-            <Text style={styles.loginText}>Welcome Back</Text>
-            <TextInput style={styles.inputBox} placeholder='Username here...'></TextInput>
+
+
+
+          <View style={styles.logoContainer}>
+            <Text>Welcome Back</Text>
+              <Image></Image>
+          </View>
+
+          <View style={styles.loginbuttons}>
+            <TouchableOpacity style={styles.loginbtn}>
+              <Text style={styles.btnText}>SignIn</Text>
+            </TouchableOpacity>
+          </View>
+
+
+            {/* <View style={styles.loginTextContainer}>
+            </View>
+            <View style={styles.inputContainer}>
+                            <Text style={styles.loginText}>Welcome Back</Text>
+
+              <TextInput style={styles.inputBox} placeholder='Username here...'></TextInput>
             <TextInput style={styles.inputBox} placeholder='Password here...'></TextInput>
-            <TouchableOpacity style={styles.loginBtn} >
-                    <Text style={{fontSize:20,fontWeight:'800'}}>Sign In</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.loginBtnGoogle} >
-                    <Image  style={{width:20,height:20}} />
-                    <Text style={{fontSize:20,fontWeight:'800',paddingRight:20}}>Sign In</Text>
-            </TouchableOpacity>
-            <View style={styles.hyperLinksContainer}>
+            </View>
+            <View style={styles.loginbuttons}>
+              <TouchableOpacity style={styles.loginBtn} >
+                      <Text style={{fontSize:20,fontWeight:'800'}}>Sign In</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.loginBtnGoogle} >
+                      <Image  style={{width:20,height:20}} />
+                      <Text style={{fontSize:20,fontWeight:'800',paddingRight:20}}>Sign In</Text>
+              </TouchableOpacity>
+            </View> */}
+            {/* <View style={styles.hyperLinksContainer}>
               <TouchableOpacity>
                 <Text style={styles.hyperLink}>Create Account</Text>
                 </TouchableOpacity>  
               <TouchableOpacity>
                 <Text style={styles.hyperLink}>Forgot Password?</Text>
               </TouchableOpacity>
-            </View>
+            </View> */}
     </KeyboardAvoidingView>
   );
 }
@@ -39,57 +62,89 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  logo:{
-    flex:2,
-    fontSize:90,
-    backgroundColor:"#4b16ddff"
+  logoContainer:{
+    flex:3,
+    backgroundColor:'#cd2d2dff',
   },
-  loginText:{
-    color:'black',
-    fontWeight:'bold',
-    textAlign:'center',
-    fontSize:30,
-  },
-  inputBox:{
-    height:40,
-    width:deviceWidth-50,
-    marginTop:20,
-    backgroundColor:'#dededeee',
-    paddingHorizontal:20,
-    borderRadius:6,
-  },
-  hyperLinksContainer:{
-    width:deviceWidth-50,
-    display:'flex',
-    flexDirection:'row',
-    justifyContent:'space-between',
-    marginTop:20,
-  },
-  hyperLink:{
-    textAlign:'center',
-    color:'blue',
-  },
-  loginBtn:{
-    justifyContent:'center',
-    alignItems:'center',
-    backgroundColor:'orange',
-    height:35,
-    width:deviceWidth-50,
-    textAlign:'center',
-    borderRadius:8,
-    marginTop:20,
-  },
-  loginBtnGoogle:{
-    flexDirection:'row',
-    alignItems:'center',
-    justifyContent:'center',
-    backgroundColor:'white',
-    height:35,
-    width:deviceWidth-50,
-    textAlign:'center',
-    borderRadius:8,
-    marginTop:20,
+  loginbuttons:{
+    flex:1,
+    paddingVertical:30,
   },
 
-  
+
+  loginbtn:{
+    justifyContent:'center',
+    alignItems:"center",
+    borderWidth:2,
+    borderRadius:50,
+    width:deviceWidth-150,
+
+    
+  },
+  btnText:{
+    paddingHorizontal:20,
+    paddingVertical:20
+  }
+
 });
+//   loginTextContainer:{
+//     flex:2
+//   },
+//     loginText:{
+//     color:'black',
+//     fontWeight:'bold',
+//     textAlign:'center',
+//     fontSize:30,
+//     paddingTop:30
+//   },
+//   inputContainer:{
+//     flex:4,
+//     backgroundColor:'#b41515ff'
+//   },
+//   inputBox:{
+  
+//     height:40,
+//     width:deviceWidth-50,
+//     marginTop:20,
+//     backgroundColor:'#dededeee',
+//     paddingHorizontal:20,
+//     borderBottomWidth:2
+//   },
+//   hyperLinksContainer:{
+//     width:deviceWidth-50,
+//     display:'flex',
+//     flexDirection:'row',
+//     justifyContent:'space-between',
+//     marginTop:20,
+//   },
+//   hyperLink:{
+//     textAlign:'center',
+//     color:'blue',
+//   },
+//   loginBtn:{
+//     justifyContent:'center',
+//     alignItems:'center',
+//     backgroundColor:'orange',
+//     height:35,
+//     width:deviceWidth-50,
+//     textAlign:'center',
+//     borderRadius:8,
+//     marginTop:20,
+//   },
+//   loginBtnGoogle:{
+//     flexDirection:'row',
+//     alignItems:'center',
+//     justifyContent:'center',
+//     backgroundColor:'white',
+//     height:35,
+//     width:deviceWidth-50,
+//     textAlign:'center',
+//     borderRadius:8,
+//     marginTop:20,
+//   },
+//   loginbuttons:{
+//     flex:2
+//   }
+
+  
+// 
