d87f619500bc9300bfe16b565e7999cb3b5967a0
TedHopewell <hopewellmaome@gmail.com>
2025-09-30 23:33:57 +0200

    progress on the landing page

--- src/events.tsx
+++ src/events.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View,TextInput,TouchableOpacity,Image,Dimensions, KeyboardAvoidingView, } from 'react-native';
+import themecolors from "../themes/themecolors";
 
 // import google from "./assets/google.png";
 // import { StatusBar } from 'expo-status-bar';
@@ -10,13 +11,16 @@ export default function EventsScreen() {
     <KeyboardAvoidingView style={styles.container} behavior="padding">
       <View style={styles.topContainer}>
          <Text style={styles.Logopart}>E.</Text>
-         <Text style={styles.welcometext}>Welcome back<span style={styles.name}>name</span></Text>
-         <View style={styles.profilePic}>
-          
+         <View style={styles.topTextContainer}>
+                       <Text style={styles.welcometext}>Welcome back</Text><Text style={styles.name}>Hopewell</Text>
+
          </View>
+         <Image style={styles.profilePic}>
+          
+         </Image>
       </View>
       <View style={styles.middleContainer}>
-
+        
       </View>
       <View style={styles.bottomContainer}>
 
@@ -31,11 +35,53 @@ const deviceWidth = Math.round(Dimensions.get('window').width);
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'rgba(192, 230, 204, 0.6)',
+    backgroundColor: themecolors.primary,
     alignItems: 'center',
     justifyContent: 'center',
   },
   topContainer:{
     flex:1,
-  }
+    flexDirection:"row",
+    paddingTop:5,
+    justifyContent:'space-around',
+    alignItems:'center',
+    width:deviceWidth,
+
+    
+  },
+  Logopart:{
+    fontWeight:'bold',
+    fontSize:40,
+    color:themecolors.accent,
+  },
+  topTextContainer:{
+    flexDirection:'column',
+    justifyContent:'center',
+    alignItems:'center',
+    gap:3,
+  },
+
+  welcometext:{
+    fontSize:20,
+    fontWeight:'800',
+    color:themecolors.text
+  },
+  name:{
+    fontSize:20
+  },
+  profilePic:{
+    height:50,
+    width:50,
+    borderRadius:50,
+    backgroundColor:themecolors.accent,
+  },
+
+  middleContainer:{
+    flex:5,
+    backgroundColor:"red",
+  },
+  bottomContainer:{
+    flex:1,
+    backgroundColor:"green"
+  },
 });
---
+++ themes/themecolors.tsx
@@ -0,0 +1,8 @@
+export default {
+  primary: "#F4F6F9",
+  primaryLight: "#FAFAFA",
+  primaryDark: "#0056B3",
+  accent: "#00C853",
+  background: "#F4F6F9",
+  text: "#212121",
+};
